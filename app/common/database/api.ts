54ef9809651a7b9a14f075e499fbbc40b3c32e79
Nelson Kouame <kouamenelson47@gmail.com>
2026-01-29 03:44:15 +0100

    add url original

--- app/common/database/api.ts
+++ app/common/database/api.ts
@@ -1,8 +1,8 @@
 // common/database/api.ts
 import axios from "axios";
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001/api/v1",
-  // baseURL: "https://actufoody-backend-production.up.railway.app/api/v1",
+  // baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001/api/v1",
+  baseURL: "https://actufoody-backend-production.up.railway.app/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
